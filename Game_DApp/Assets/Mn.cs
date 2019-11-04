82fae532bd5272b5f57cc1d9799fec24d27d3801
developerYHL <developer.yhl@gmail.com>
2019-11-04 17:57:30 +0900

    .net 4.x Upgrade

---
+++ Game_DApp/Assets/Mn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NBitcoin;
+using UnityEngine.UI;
+
+public class Mn : MonoBehaviour
+{
+    public Text seedText;
+    public string password;
+
+    [Space]
+    public string seedWord;
+    public string newPassword;
+
+    Mnemonic mnemo;
+    ExtKey hdRoot;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mnemo = new Mnemonic(Wordlist.English, WordCount.Twelve);
+        hdRoot = mnemo.DeriveExtKey(password);
+        seedText.text = mnemo.ToString();
+        Debug.Log(mnemo);
+        Debug.Log(hdRoot.PrivateKey.PubKey);
+    }
+
+    public void MnemoEnter()
+    {
+        mnemo = new Mnemonic(seedWord, Wordlist.English);
+        hdRoot = mnemo.DeriveExtKey(newPassword);
+
+        Debug.Log("New mnemo:" + mnemo);
+        Debug.Log("New PrivateKey:" + hdRoot.PrivateKey.PubKey);
+    }
+}
